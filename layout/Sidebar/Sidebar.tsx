fab3d778a1334403c7e76ed9947a007e34675a2c
Nikita <bananchik26@gmail.com>
2021-09-30 12:31:25 +0300

    1

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,16 @@
 import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.css';
 import cn from 'classnames';
+import React, { useState } from 'react';
+import { Rating } from '../../components/Rating/Rating';
 
 export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+    const [rating, setRating] = useState<number>(4);
+
     return (
         <div {...props}>
             Sidebar
+            <Rating rating={rating} isEditable={true} setRating={setRating} />
         </div>
     );
 };
